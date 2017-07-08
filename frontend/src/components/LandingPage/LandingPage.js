7c91067a385e37912f0c81252d63003986a1aea7
isaackps <isaac.kps@gmail.com>
2017-07-08 21:06:09 +0800

    doing the landing page

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -1,13 +1,32 @@
 import React, {PropTypes} from 'react';
 
+// import css files
+import "./css/style.css";
+import "./css/simple-line-icons.css";
+//import "./css/owl.theme.default.min.css";
+//import "./css/owl.carousel.min.css";
+import "./css/magnific-popup.css";
+import "./css/icomoon.css";
+import "./css/animate.css";
+
+
 export default class LandingPage extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    return (<div>MyComponent</div>);
+    return (
+      <div className="container-fluid">
+        MyComponent
+      </div>
+    );
   }
+
+  componentDidMount() {
+
+  }
+
 }
 
 LandingPage.propTypes = {
